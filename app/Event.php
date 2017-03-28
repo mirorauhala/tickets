37871d65a179c0e740174629562d7c106aa059f4
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-03-28 20:23:27 +0300

    add event model

---
+++ app/Event.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tikematic;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'location', 'details', 'url',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
