453c2272cac65f47f3e66122fff4c2e604677515
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-03-28 19:52:09 +0300

    get the user full name

--- app/User.php
+++ app/User.php
@@ -26,4 +26,13 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Returns user full name.
+     *
+     * @return string
+     */
+    public function full_name() {
+        return $this->first_name . " " . $this->last_name;
+    }
 }
