89d01185e5212a46afeb0a6c08efc3fc816cfca9
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-03-29 19:52:40 +0300

    use new base layout

--- resources/views/layouts/event.blade.php
+++ resources/views/layouts/event.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.base')
 
 @section('base.content')
 <div class="container">
--- resources/views/tickets/all.blade.php
+++ resources/views/tickets/all.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
+@extends('layouts.base')
 
-@section('content')
+@section('base.content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
