d81a4a23d0bc255d754ea6cc37c9b014a5d5f82a
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-03-29 19:38:22 +0300

    add new links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,9 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;
+                        <li><a href="{{ route('events.details') }}">Event</a></li>
+                        <li><a href="{{ route('tickets') }}">Tickets</a></li>
+                        <li><a href="{{ route('tournaments') }}">Tournaments</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
