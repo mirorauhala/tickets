6f314b617c1b94327bf8dc414ac59b34a92b9242
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-03-29 19:48:37 +0300

    add new layout

---
+++ resources/views/layouts/event.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('base.content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    You don't participate in any tournaments.
+                </div>
+            </div>
+        </div>
+
+        @yield('content')
+    </div>
+</div>
+@endsection
