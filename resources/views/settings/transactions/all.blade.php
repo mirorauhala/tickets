7da5fcac649599cf25eb228991eaa8573ef300e9
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-03-30 15:54:59 +0300

    use new settings layout

--- resources/views/settings/transactions/all.blade.php
+++ resources/views/settings/transactions/all.blade.php
@@ -1,48 +1,37 @@
-@extends('layouts.base')
+@extends('layouts.settings')
 
-@section('base.content')
-<div class="row">
-    <div class="col-md-3">
-        @include("partials.nav.settings")
+@section('settings.title', __('settings.panel.title.transactions'))
+
+@section('content')
+
+@if(count(Auth::user()->transactions) > 0)
+    <div class="table-responsive">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <td>{{ __('settings.transactions.id') }}</td>
+                    <td>{{ __('settings.transactions.title') }}</td>
+                    <td>{{ __('settings.transactions.payer') }}</td>
+                    <td>{{ __('settings.transactions.cost') }}</td>
+                    <td>{{ __('settings.transactions.currency') }}</td>
+                    <td>{{ __('settings.transactions.vat') }}</td>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach(Auth::user()->transactions as $transaction)
+                    <tr>
+                        <td>{{ $transaction->id }}</td>
+                        <td>{{ $transaction->title }}</td>
+                        <td>{{ $transaction->payer->first()->full_name() }}</td>
+                        <td>{{ Helper::decimalMoneyFormatter($transaction->cost, $transaction->currency) }}</td>
+                        <td>{{ $transaction->currency }}</td>
+                        <td>{{ $transaction->vat }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
-
-    <div class="col-md-9">
-        <div class="panel panel-default">
-            <div class="panel-heading">{{ __('settings.panel.title.transactions') }}</div>
-
-            <div class="panel-body">
-                @if(count(Auth::user()->transactions) > 0)
-                    <div class="table-responsive">
-                        <table class="table table-bordered">
-                            <thead>
-                                <tr>
-                                    <td>{{ __('settings.transactions.id') }}</td>
-                                    <td>{{ __('settings.transactions.title') }}</td>
-                                    <td>{{ __('settings.transactions.payer') }}</td>
-                                    <td>{{ __('settings.transactions.cost') }}</td>
-                                    <td>{{ __('settings.transactions.currency') }}</td>
-                                    <td>{{ __('settings.transactions.vat') }}</td>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach(Auth::user()->transactions as $transaction)
-                                    <tr>
-                                        <td>{{ $transaction->id }}</td>
-                                        <td>{{ $transaction->title }}</td>
-                                        <td>{{ $transaction->payer->first()->full_name() }}</td>
-                                        <td>{{ Helper::decimalMoneyFormatter($transaction->cost, $transaction->currency) }}</td>
-                                        <td>{{ $transaction->currency }}</td>
-                                        <td>{{ $transaction->vat }}</td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                @else
-                    <p>{{ __('settings.transctions.no-transactions') }}</p>
-                @endif
-            </div>
-        </div>
-    </div>
-</div>
+@else
+    <p>{{ __('settings.transctions.no-transactions') }}</p>
+@endif
 @endsection
