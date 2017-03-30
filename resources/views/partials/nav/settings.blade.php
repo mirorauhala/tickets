82e6640b0fcad34adfdf55c7c05fb75e20da91aa
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-03-30 15:50:37 +0300

    add settings navigation partial

---
+++ resources/views/partials/nav/settings.blade.php
@@ -0,0 +1,14 @@
+<ul class="nav nav-pills nav-stacked">
+    <li role="presentation" class="{{ Helper::route_active(['settings.profile']) }}">
+        <a href="{{ route('settings.profile') }}">{{ __('settings.nav.profile') }}</a>
+    </li>
+    <li role="presentation" class="{{ Helper::route_active(['settings.password']) }}">
+        <a href="{{ route('settings.password') }}">{{ __('settings.nav.change-password') }}</a>
+    </li>
+    <li role="presentation" class="{{ Helper::route_active(['settings.language']) }}">
+        <a href="{{ route('settings.language') }}">{{ __('settings.nav.language') }}</a>
+    </li>
+    <li role="presentation" class="{{ Helper::route_active(['settings.transactions*']) }}">
+        <a href="{{ route('settings.transactions.all') }}">{{ __('settings.nav.transactions') }}</a>
+    </li>
+</ul>
