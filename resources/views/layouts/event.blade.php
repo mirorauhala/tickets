ebce50a21eb126a23eda58fac846ab146d6e9cbd
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-03-30 14:51:33 +0300

    add event name, location, and links

--- resources/views/layouts/event.blade.php
+++ resources/views/layouts/event.blade.php
@@ -3,15 +3,22 @@
 @section('base.content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    You don't participate in any tournaments.
-                </div>
-            </div>
-        </div>
+        <div class="col-md-12">
+            <h1 class="text-center">Connection Lan: eSports 2017</h1>
 
-        @yield('content')
+            <p class="text-center lead">Alaseinäjoenkatu 15, 60220 Seinäjoki</p>
+
+            <p class="text-center">
+                <a role="presentation" class="btn btn-sm btn-primary" href="{{ route('events.details') }}">Details</a></li>
+                <a role="presentation" class="btn btn-sm btn-link" href="#">Maps</a></li>
+                <a role="presentation" class="btn btn-sm btn-link" href="#">Tickets</a></li>
+                <a role="presentation" class="btn btn-sm btn-link" href="#">Tournaments</a></li>
+            </p>
+
+            <br>
+
+            @yield('content')
+        </div>
     </div>
 </div>
 @endsection
