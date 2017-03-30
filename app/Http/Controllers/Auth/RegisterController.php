809a73db367218327c881b89950fe8738cedaa08
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-03-30 21:03:56 +0300

    password is automatically hashed by mutator

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,7 +67,7 @@ class RegisterController extends Controller
             'first_name' => $data['first_name'],
             'last_name' => $data['last_name'],
             'email' => $data['email'],
-            'password' => bcrypt($data['password']),
+            'password' => $data['password'],
         ]);
     }
 }
