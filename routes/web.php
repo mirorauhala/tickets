6d4fce1e621c62d5aad53309978101424427356b
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-03-30 15:42:22 +0300

    add settings routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ Auth::routes();
 Route::get('/', 'EventController@index')->name('events.details');
 Route::get('/tickets', 'TicketController@index')->name('tickets');
 Route::get('/tournaments', 'TournamentController@index')->name('tournaments');
+
+
+/*
+|--------------------------------------------------------------------------
+| Settings
+|--------------------------------------------------------------------------
+*/
+Route::get('/settings', 'SettingsController@showSettings')->name('settings.profile');
+Route::post('/settings', 'SettingsController@updateProfile');
+
+Route::get('/settings/password', 'SettingsController@showPasswordForm')->name('settings.password');
+Route::post('/settings/password', 'SettingsController@updatePassword');
+
+Route::get('/settings/language', 'SettingsController@showLanguages')->name('settings.language');
+Route::post('/settings/language', 'SettingsController@updateLanguage');
+
+Route::get('/settings/transactions', 'SettingsController@showTransactions')->name('settings.transactions.all');
+Route::get('/settings/transactions/{transaction}', 'SettingsController@showSpecificTransaction')->name('settings.transactions.specific');
