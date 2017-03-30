466163fef8156b7f1cb2d3c30e9ffc1dd2540f2d
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-03-30 20:25:45 +0300

    add basic ticket layout

--- resources/views/events/tickets.blade.php
+++ resources/views/events/tickets.blade.php
@@ -4,17 +4,51 @@
 
 <div class="row">
     <div class="col-md-12">
+
+        <h1 class="text-center">Tickets</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-4">
         <div class="panel panel-default">
             <div class="panel-body">
-                <div class="col-md-8 col-md-offset-2">
-                    <div class="panel-body">
-                        <h1>Tickets</h1>
-
-                    </div>
+                <div class="panel-heading">Visitor</div>
+                <div class="panel-body">
+                    <ul>
+                        <li>Unrestricted access to the event venue.</li>
+                    </ul>
                 </div>
             </div>
         </div>
     </div>
+
+    <div class="col-md-4">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="panel-heading">Basic Gamer</div>
+                <div class="panel-body">
+                    <ul>
+                        <li>Unrestricted access to the event venue.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="panel-heading">Pro Gamer</div>
+                <div class="panel-body">
+                    <ul>
+                        <li>Unrestricted access to the event venue.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
 
 @endsection
