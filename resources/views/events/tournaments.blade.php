9fba5aa1fdb2c8104ce90d05de98c9d0872f2f65
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-03-30 16:29:15 +0300

    add views for maps, tickets, and tournaments

---
+++ resources/views/events/tournaments.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.event')
+
+@section('content')
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel-body">
+                        <h1>Tournaments</h1>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
