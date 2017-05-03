03f150f2de3eefd2b01d17d409391597b74b9621
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-05-03 15:43:47 +0300

    render tickets properly

--- resources/views/events/tickets.blade.php
+++ resources/views/events/tickets.blade.php
@@ -4,35 +4,45 @@
 
 <div class="row">
     <div class="col-md-12">
-
         <h1 class="text-center">Tickets</h1>
     </div>
 </div>
 
-<div class="row">
+<br>
 
-    @if(count($tickets) > 0)
-        @foreach($tickets as $ticket)
-        <div class="col-md-4">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <div class="panel-heading">{{ $ticket->name }}</div>
-                    <div class="panel-body">
-                        <ul>
-                            <li>{{ Helper::decimalMoneyFormatter($ticket->price, $ticket->currency) }}</li>
-                            <li>Unrestricted access to the event venue.</li>
-                        </ul>
-                        <br>
-                        <p><a href="#" class="btn btn-primary">Buy</a></p>
-                    </div>
-                </div>
-            </div>
+@if(count($tickets) > 0)
+    @foreach($tickets as $ticket)
+    <div class="row ticket">
+        <div class="col-sm-4 col-xs-9">
+            <h5>{{ $ticket->name }}</h5>
+            <ul>
+                <li>Cool feature no. 1 you'll get by choosing this ticket.</li>
+                <li>Cool feature no. 2 you'll get by choosing this ticket.</li>
+            </ul>
         </div>
-        @endforeach
-    @else
-        <p class="text-center">There are no tickets to show.</p>
-    @endif
+        <div class="col-sm-4 col-xs-3 text-right">
+            {{ Helper::decimalMoneyFormatter($ticket->price, $ticket->currency) }}
+        </div>
+        <div class="col-md-2 col-md-offset-2 col-sm-4 col-sm-offset-0 col-xs-5 col-xs-offset-7">
+            Amount
+        </div>
+    </div>
+    @endforeach
 
-</div>
+    <div class="row ticket">
+        <div class="col-sm-4 col-xs-9">
+
+        </div>
+        <div class="col-sm-4 col-xs-3 text-right">
+
+        </div>
+        <div class="col-md-2 col-md-offset-2 col-sm-4 col-sm-offset-0 col-xs-5 col-xs-offset-7">
+            <input type="submit" class="btn btn-block btn-primary" value="Purchase">
+        </div>
+    </div>
+@else
+    <p class="text-center">There are no tickets to show.</p>
+@endif
+
 
 @endsection
