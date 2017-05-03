91112dadb3bc19b36426bfa08018458ed6076bd5
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-05-03 15:11:15 +0300

    make sidebar scrollable when viewport height gets too small

--- resources/views/partials/nav/sidebar.blade.php
+++ resources/views/partials/nav/sidebar.blade.php
@@ -1,38 +1,44 @@
 <section class="sidebar">
+    <div class="sidebar-scrollable">
+        <div class="sidebar-contents">
 
-    <a href="#" class="sidebar-logo"><image src="/images/tikematic.SVG" height="30" alt="Tikematic"></a>
-    <ul class="nav nav-pills nav-stacked">
-        <li class="nav-title">Featured event</li>
-        <li class="{{ Helper::route_active(['events*']) }}"><a href="{{ route('events.details') }}">Connection Lan: eSports 2017</a></li>
-    </ul>
+            <a href="#" class="sidebar-logo"><image src="/images/tikematic.SVG" height="30" alt="Tikematic"></a>
+            <ul class="nav nav-pills nav-stacked">
+                <li class="nav-title">Featured event</li>
+                <li class="{{ Helper::route_active(['events*']) }}"><a href="{{ route('events.details') }}">Connection Lan: eSports 2017</a></li>
+            </ul>
 
-    <ul class="nav nav-pills nav-stacked">
-        <li class="nav-title">Personal</li>
-        <li class="{{ Helper::route_active(['tickets*']) }}"><a href="{{ route('tickets') }}">My Tickets</a></li>
-        <li class="{{ Helper::route_active(['tournaments*']) }}"><a href="{{ route('tournaments') }}">My Tournaments</a></li>
-    </ul>
+            <ul class="nav nav-pills nav-stacked">
+                <li class="nav-title">Personal</li>
+                <li class="{{ Helper::route_active(['tickets*']) }}"><a href="{{ route('tickets') }}">My Tickets</a></li>
+                <li class="{{ Helper::route_active(['tournaments*']) }}"><a href="{{ route('tournaments') }}">My Tournaments</a></li>
+            </ul>
 
-    <ul class="nav nav-pills nav-stacked">
-        <li class="nav-title">Account</li>
-        <li class="{{ Helper::route_active(['settings*']) }}"><a href="{{ route('settings.profile') }}">Profile</a></li>
-        <li class="{{ Helper::route_active(['settings*']) }}"><a href="{{ route('settings.profile') }}">Settings</a></li>
+            <div class="sidebar-nav-bottom">
+                <ul class="nav nav-pills nav-stacked">
+                    <li class="nav-title">Account</li>
+                    <li class="{{ Helper::route_active(['settings*']) }}"><a href="{{ route('settings.profile') }}">Profile</a></li>
+                    <li class="{{ Helper::route_active(['settings*']) }}"><a href="{{ route('settings.profile') }}">Settings</a></li>
 
-        @if (Auth::guest())
-            <li class="{{ Helper::route_active(['login*']) }}"><a href="{{ route('login') }}">Log In</a></li>
-        @else
-            <li>
-                <a href="{{ route('logout') }}"
-                    onclick="event.preventDefault();
-                            document.getElementById('logout-form').submit();">
-                    Log out
-                </a>
+                    @if (Auth::guest())
+                        <li class="{{ Helper::route_active(['login*']) }}"><a href="{{ route('login') }}">Log In</a></li>
+                    @else
+                        <li>
+                            <a href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                                Log out
+                            </a>
 
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    {{ csrf_field() }}
-                </form>
-            </li>
-        @endif
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </li>
+                    @endif
 
-    </ul>
+                </ul>
+            </div>
+        </div>
+    </div>
 
 </section>
