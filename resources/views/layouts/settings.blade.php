805af44fd9d8a07827c2037ca382cb4abf68454b
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-05-03 15:44:14 +0300

    use sidebar on all pages

--- resources/views/layouts/settings.blade.php
+++ resources/views/layouts/settings.blade.php
@@ -4,32 +4,34 @@
 
 @section('base.content')
 
-@include('partials.nav.bar')
+@include('partials.nav.sidebar')
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h1>Settings</h1>
+<section class="application">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Settings</h1>
 
-            <br>
+                <br>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-md-3">
-            @include("partials.nav.settings")
-        </div>
+        <div class="row">
+            <div class="col-md-3">
+                @include("partials.nav.settings")
+            </div>
 
-        <div class="col-md-9">
-            <div class="panel panel-default">
-                <div class="panel-heading">@yield('settings.title', 'Untitled')</div>
+            <div class="col-md-9">
+                <div class="panel panel-default">
+                    <div class="panel-heading">@yield('settings.title', 'Untitled')</div>
 
-                <div class="panel-body">
+                    <div class="panel-body">
 
-                    @yield('content')
+                        @yield('content')
 
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</section>
 @endsection
--- resources/views/layouts/tournaments.blade.php
+++ resources/views/layouts/tournaments.blade.php
@@ -4,18 +4,19 @@
 
 @section('base.content')
 
-@include('partials.nav.bar')
+@include('partials.nav.sidebar')
+<section class="application">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Tournaments</h1>
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h1>Tournaments</h1>
-
-            <br>
+                <br>
+            </div>
+        </div>
+        <div class="row">
+            @yield('content')
         </div>
     </div>
-    <div class="row">
-        @yield('content')
-    </div>
-</div>
+</section>
 @endsection
