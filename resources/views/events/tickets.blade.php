c4d005fc4e282f691c3979d0ee0432a14ef14378
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-05-12 17:18:14 +0300

    loop through all event's tickets

--- resources/views/events/tickets.blade.php
+++ resources/views/events/tickets.blade.php
@@ -11,35 +11,40 @@
 <br>
 
 @if(count($tickets) > 0)
-    @foreach($tickets as $ticket)
-    <div class="row ticket">
-        <div class="col-sm-4 col-xs-9">
-            <h5>{{ $ticket->name }}</h5>
-            <ul>
-                <li>Cool feature no. 1 you'll get by choosing this ticket.</li>
-                <li>Cool feature no. 2 you'll get by choosing this ticket.</li>
-            </ul>
-        </div>
-        <div class="col-sm-4 col-xs-3 text-right">
-            {{ Helper::decimalMoneyFormatter($ticket->price, $ticket->currency) }}
-        </div>
-        <div class="col-md-2 col-md-offset-2 col-sm-4 col-sm-offset-0 col-xs-5 col-xs-offset-7">
-            <input type="number" min="0" max="{{ $ticket->maxAmountPerTransaction }}" value="0">
-        </div>
-    </div>
-    @endforeach
+    <form method="POST" action="{{ route('order.view') }}">
+        {{ csrf_field() }}
+        @foreach($tickets as $key=>$ticket)
+            <div class="row ticket">
+                <div class="col-sm-4 col-xs-9">
+                    <h2>{{ $ticket->name }}</h2>
+                    <ul>
+                        <li>Cool feature no. 1 you'll get by choosing this ticket.</li>
+                        <li>Cool feature no. 2 you'll get by choosing this ticket.</li>
+                    </ul>
+                </div>
+                <div class="col-sm-4 col-xs-3 text-right">
+                    {{ Helper::decimalMoneyFormatter($ticket->price, $ticket->currency) }}
+                </div>
+                <div class="col-md-2 col-md-offset-2 col-sm-4 col-sm-offset-0 col-xs-5 col-xs-offset-7">
+                    <input type="hidden" name="tickets[{{ $key }}][id]" value="{{ $ticket->id }}">
+                    <input type="number" name="tickets[{{ $key }}][amount]" min="0" max="{{ $ticket->maxAmountPerTransaction }}" value="0">
+                </div>
+            </div>
+        @endforeach
 
-    <div class="row ticket">
-        <div class="col-sm-4 col-xs-9">
+        <div class="row ticket">
+            <div class="col-sm-4 col-xs-9">
 
-        </div>
-        <div class="col-sm-4 col-xs-3 text-right">
+            </div>
+            <div class="col-sm-4 col-xs-3 text-right">
 
+            </div>
+            <div class="col-md-2 col-md-offset-2 col-sm-4 col-sm-offset-0 col-xs-5 col-xs-offset-7">
+                <input type="submit" class="btn btn-block btn-primary" value="Purchase">
+            </div>
         </div>
-        <div class="col-md-2 col-md-offset-2 col-sm-4 col-sm-offset-0 col-xs-5 col-xs-offset-7">
-            <input type="submit" class="btn btn-block btn-primary" value="Purchase">
-        </div>
-    </div>
+
+    </form>
 @else
     <p class="text-center">There are no tickets to show.</p>
 @endif
