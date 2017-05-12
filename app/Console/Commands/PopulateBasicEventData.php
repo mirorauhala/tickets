be0342e21872ff826bd63b13640e37b7cf688172
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-05-12 17:19:17 +0300

    update data population command

--- app/Console/Commands/PopulateBasicEventData.php
+++ app/Console/Commands/PopulateBasicEventData.php
@@ -37,19 +37,45 @@ class PopulateBasicEventData extends Command
      */
     public function handle()
     {
-        factory(\Tikematic\Event::create([
+        $event = factory(\Tikematic\Event::class)->create([
             'name' => 'Connection Lan: eSports 2017',
             'location' => 'Alaseinäjoenkatu 15, 60220 Seinäjoki, Finland',
             'details' => 'Welcome to the ticket sales for Connection Lan: eSports 2017. In here you shall find event...',
             'url' => "http://connectionlan.fi",
-        ]));
+        ]);
 
-        factory(\Tikematic\User::create([
+        $map = $event->maps()->create([
+            'name' => 'First floor',
+            'description' => 'This map actually is located on the second floor but we chose to keep it easier for people to understand that the event\'s maps don\'t start from the second floor and wonder where the first floor is.',
+        ]);
+
+
+        // first row of seats
+        for($i = 1; $i <= 15; $i++) {
+            $map->seats()->create([
+                'name' => '1A' . $i,
+                'status' => 'available',
+                'top' => 50,
+                'left' => 20 * $i + 150,
+            ]);
+        }
+
+        // second row of seats
+        for($i = 1; $i <= 15; $i++) {
+            $map->seats()->create([
+                'name' => '1A' . ($i + 15),
+                'status' => 'available',
+                'top' => 70,
+                'left' => 20 * $i + 150,
+            ]);
+        }
+
+        factory(\Tikematic\User::class)->create([
             'first_name' => 'john',
             'last_name' => 'doe',
             'email' => 'john.doe@email.com',
             'password' => 'secret', // THIS IS FOR TESTING PURPOSES, NOT PRODUCTION
             'superuser' => true,
-        ]));
+        ]);
     }
 }
