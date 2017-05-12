826a25e4f14a6f01396a2eae21303c5d18074301
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-05-12 17:17:27 +0300

    add order view

---
+++ resources/views/layouts/order.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.base')
+
+@section('base.title', 'Order')
+
+@section('base.content')
+
+@include('partials.nav.sidebar')
+
+<section class="application">
+    <section class="application-header">
+        <div class="container">
+            <h1>{{ __('order.header') }}</h1>
+        </div>
+    </section>
+
+    <div class="container">
+        <div class="row">
+            @yield('content')
+        </div>
+    </div>
+</section>
+@endsection
