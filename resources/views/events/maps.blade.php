1755a7c128845ce00f211cffaeeeb52d5562461d
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-05-12 17:11:16 +0300

    loop through all maps

--- resources/views/events/maps.blade.php
+++ resources/views/events/maps.blade.php
@@ -2,19 +2,22 @@
 
 @section('content')
 
-<div class="row">
-    <div class="col-md-12">
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <div class="col-md-8 col-md-offset-2">
-                    <div class="panel-body">
-                        <h1>Maps</h1>
-
+<h1>Maps</h1>
+@if(count($maps) > 0)
+    @foreach($maps as $map)
+        <div class="row ticket">
+            <div class="col-md-8">
+                <a class="thumbnail clearfix" href="{{ route('events.map', ['map' => $map]) }}">
+                    <div class="col-md-12">
+                        <h2>{{ $map->name }} <small>{{ count($map->seats()->status('taken')->get() ) }}/{{ count($map->seats()->status('available')->get()) }} seats</small></h2>
+                        <p>{{ $map->description }}</p>
                     </div>
-                </div>
+                </a>
             </div>
         </div>
-    </div>
-</div>
+    @endforeach
+@else
+    <p>There are no tickets to show.</p>
+@endif
 
 @endsection
