25421c33b99d2e38c6016dfbc41b1858e8822599
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-05-12 17:11:47 +0300

    add specific map view, still needs rework in html side

---
+++ resources/views/events/map.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.event')
+
+@section('content')
+
+<h1>Maps / {{ $map->name }}</h1>
+<p>{{ $map->description }}</p>
+
+</div>
+</div>
+</div>
+<div class="map" style="height: 500px">
+
+    @foreach($seats as $seat)
+        <div class="seat seat-{{ $seat->status }}" style="height: 20px; width: 20px; top: {{ $seat->top }}px; left: {{ $seat->left }}px;"></div>
+    @endforeach
+
+
+@endsection
