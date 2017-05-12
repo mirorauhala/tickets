16e6c992f1ca6bdf688d8df5bf14711611bd2bef
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-05-12 17:06:00 +0300

    create transaction model and migration

---
+++ app/Transaction.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tikematic;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+class Transation extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'price', 'vat', 'currency', 'standalone', 'availableAt', 'unavailableAt', 'event_id',
+    ];
+
+    protected $dates = [
+        'availableAt',
+        'unavailableAt',
+    ];
+
+
+    /**
+     * Scope a query to only include available tickets.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeAvailable($query)
+    {
+        return $query->where('availableAt', '<', Carbon::now())
+            ->where('unavailableAt', '>', Carbon::now());
+    }
+}
---
+++ database/migrations/2017_05_09_161316_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->integer('code')->unsigned();
+            $table->string('status');
+            $table->integer('total');
+            $table->string('currency');
+            $table->integer('vat')->unsigned();
+            $table->dateTimeTz('pending_lock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
