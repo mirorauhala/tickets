ebd912dfbac14d632d5604348b4bf6571bd79d56
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-05-12 17:16:47 +0300

    add english translations

---
+++ resources/lang/en/order.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Order Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used within the user settings.
+    |
+    */
+
+    'header' => 'New Order',
+
+];
