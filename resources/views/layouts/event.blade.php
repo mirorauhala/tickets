9738529fb8dc6c6567ed0fe9a6f444218cc7bfe7
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-05-20 20:20:55 +0300

    use event's own data

--- resources/views/layouts/event.blade.php
+++ resources/views/layouts/event.blade.php
@@ -11,10 +11,10 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h1 class="text-center">Connection Lan</h1>
+                <h1 class="text-center">{{ $event->name }}</h1>
                 <p class="text-center lead">
-                    Ankkalinnantie 13, 131313 Seinäjoki, Finland
-                    <a target="_blank" rel="noopener" href="https://maps.google.com/?q=OmaSp+Stadion,+Alaseinäjoenkatu,+Seinäjoki,+Finland">
+                    {{ $event->location }}
+                    <a target="_blank" rel="noopener" href="https://maps.google.com/?q={{ $event->location }}">
                         <span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span> <span class="sr-only">View in Google Maps</span>
                     </a>
                 </p>
