aaaa196ef7c649e4e1e86f1d5256f83b9ea47bb4
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-05-21 20:03:23 +0300

    fix duplicate href attribute

--- resources/views/partials/nav/topbar-admin.blade.php
+++ resources/views/partials/nav/topbar-admin.blade.php
@@ -2,9 +2,9 @@
 <section class="application-navigation">
     <div class="application-container">
         <div class="application-navigation-links">
-            <a role="presentation" class="app-link {{ Helper::route_active(['events.admin.orders'], [], 'active') }}" href="{{ route('events.admin.orders') }}" href="{{ route('events.details') }}">{{ __('event.admin.nav.orders') }}</a>
+            <a role="presentation" class="app-link {{ Helper::route_active(['events.admin.orders'], [], 'active') }}" href="{{ route('events.admin.orders') }}">{{ __('event.admin.nav.orders') }}</a>
 
-            <a role="presentation" class="app-link {{ Helper::route_active(['events.admin.tickets*'], [], 'active') }}" href="{{ route('events.admin.tickets') }}">{{ __('event.admin.nav.tickets') }}</a>
+            <a role="presentation" class="app-link {{ Helper::route_active(['events.admin.tickets*'], [], 'active') }}" href="{{ route('events.admin.tickets.list') }}">{{ __('event.admin.nav.tickets') }}</a>
 
             <a role="presentation" class="app-link {{ Helper::route_active(['events.admin.maps*'], [], 'active') }}" href="{{ route('events.admin.maps') }}">{{ __('event.admin.nav.maps') }}</a>
 
