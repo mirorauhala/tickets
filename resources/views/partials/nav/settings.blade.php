bb010d6bbea0559641b59813a6930eb375f9d082
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-05-21 20:22:26 +0300

    update settings navigation to match new design

--- resources/views/partials/nav/settings.blade.php
+++ resources/views/partials/nav/settings.blade.php
@@ -1,14 +1,10 @@
-<ul class="nav nav-pills">
-    <li role="presentation" class="{{ Helper::route_active(['settings.profile']) }}">
-        <a href="{{ route('settings.profile') }}">{{ __('settings.nav.profile') }}</a>
-    </li>
-    <li role="presentation" class="{{ Helper::route_active(['settings.password']) }}">
-        <a href="{{ route('settings.password') }}">{{ __('settings.nav.change-password') }}</a>
-    </li>
-    <li role="presentation" class="{{ Helper::route_active(['settings.language']) }}">
-        <a href="{{ route('settings.language') }}">{{ __('settings.nav.language') }}</a>
-    </li>
-    <li role="presentation" class="{{ Helper::route_active(['settings.transactions*']) }}">
-        <a href="{{ route('settings.transactions.all') }}">{{ __('settings.nav.transactions') }}</a>
-    </li>
-</ul>
+<section class="application-navigation">
+    <div class="application-container">
+        <div class="application-navigation-links">
+            <a role="presentation" class="app-link {{ Helper::route_active(['settings.profile'], []) }}" href="{{ route('settings.profile') }}">{{ __('settings.nav.profile') }}</a>
+            <a role="presentation" class="app-link {{ Helper::route_active(['settings.password*'], []) }}" href="{{ route('settings.password') }}">{{ __('settings.nav.change-password') }}</a>
+            <a role="presentation" class="app-link {{ Helper::route_active(['settings.language*'], []) }}" href="{{ route('settings.language') }}">{{ __('settings.nav.language') }}</a>
+            <a role="presentation" class="app-link {{ Helper::route_active(['settings.transactions*'], []) }}" href="{{ route('settings.transactions.all') }}">{{ __('settings.nav.transactions') }}</a>
+        </div>
+    </div>
+</section>
