2cfdd649a84e2906b5e558a8b41b51527de585ac
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-05-22 14:19:10 +0300

    rename boolean fields to have is_* prefix, add reserved field

--- database/migrations/2017_04_17_181527_create_tickets_table.php
+++ database/migrations/2017_04_17_181527_create_tickets_table.php
@@ -18,10 +18,13 @@ class CreateTicketsTable extends Migration
             $table->string('name')->nullable();
             $table->integer('price')->unsigned();
             $table->integer('vat')->unsigned();
-            $table->boolean('standalone')->default(1);
+            $table->boolean('reserved')->unsigned()->default(0);
             $table->boolean('maxAmountPerTransaction')->unsigned()->default(5);
             $table->dateTimeTz('availableAt');
             $table->dateTimeTz('unavailableAt');
+
+            $table->boolean('is_seatable')->nullable()->default(0);
+            $table->boolean('is_sleepable')->nullable()->default(0);
 
             $table->integer('event_id')->unsigned();
             $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
