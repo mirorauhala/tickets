e59b17ad1d430d5a2eb0f7f5462f858c094a7279
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-05-22 21:12:38 +0300

    Content Security Policy disallows inline JavaScript

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta id="csrfToken" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Tikematic') }} - @yield('base.title', 'Untitled')</title>
 
@@ -15,13 +15,6 @@
     <link rel="manifest" href="/manifest.json">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" async="true">
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,900" rel="stylesheet" async="true">
-
-    <!-- Scripts -->
-    <script>
-        window.Laravel = {!! json_encode([
-            'csrfToken' => csrf_token(),
-        ]) !!};
-    </script>
 </head>
 <body>
     @yield('base.content')
