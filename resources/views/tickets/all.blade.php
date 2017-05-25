92fb40da372d195c616d88bef8ae42d92e45da6d
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-05-25 23:34:24 +0300

    add ticket-card styles

--- resources/views/tickets/all.blade.php
+++ resources/views/tickets/all.blade.php
@@ -1,15 +1,25 @@
 @extends('layouts.tickets')
 
 @section('content')
-<div class="col-md-3">
-    <div class="panel panel-default">
-        <div class="panel-heading">Ticket #1234</div>
 
-        <div class="panel-body">
-            <p>Pro ticket</p>
+<div class="row">
+@if(count($tickets) > 0)
+    @foreach($tickets as $ticket)
+        <div class="col-lg-5 col-md-6 col-sm-6">
 
-            <a class="btn btn-primary" href="#" role="button">View ticket</a>
+            <div class="ticket-card">
+                <div class="ticket-details">
+                    <h1>{{ $ticket->title }}</h1>
+                    <p class="text-muted">{{ $ticket->reference }}</p>
+                    <p>Paid and assigned</p>
+                </div>
+
+                <a href="#" class="ticket-action"><span>View ticket</span></a>
+            </div>
         </div>
-    </div>
+    @endforeach
+@else
+    <p>There are no tickets to show at this time.</p>
+@endif
 </div>
 @endsection
