ae4660f112f4e0b20c53e0234299c51d971d6068
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-06-15 16:54:22 +0300

    send errors to sentry

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -32,7 +32,9 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
-        parent::report($exception);
+        if (app()->bound('sentry') && $this->shouldReport($exception)) {
+            app('sentry')->captureException($exception);
+        }
     }
 
     /**
