e9effe194e9ef31707ec73ad0f7dfff4488c09a8
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-06-15 21:25:41 +0300

    show message if no tickets

--- resources/views/events/tickets/main.blade.php
+++ resources/views/events/tickets/main.blade.php
@@ -6,14 +6,18 @@
 
 <br>
 <div class="row">
-    @foreach($tickets as $ticket)
-        <div class="col-md-5">
-            <a class="thumbnail clearfix" href="{{ route('events.tickets.ticket', ['ticket' => $ticket->id]) }}">
-                <div class="col-md-12">
-                    <h2>{{ $ticket->name }}</h2>
-                </div>
-            </a>
-        </div>
-    @endforeach
+    @if(count($tickets) > 0)
+        @foreach($tickets as $ticket)
+            <div class="col-md-5">
+                <a class="thumbnail clearfix" href="{{ route('events.tickets.ticket', ['ticket' => $ticket->id]) }}">
+                    <div class="col-md-12">
+                        <h2>{{ $ticket->name }}</h2>
+                    </div>
+                </a>
+            </div>
+        @endforeach
+    @else
+        <p>{{ Helper::tra('event.tickets.no-tickets')}}
+    @endif
 </div>
 @endsection
