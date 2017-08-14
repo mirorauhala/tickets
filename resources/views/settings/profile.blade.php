2db13b4413dae77d8c08bccfac331cee171cb9b9
BuildTools <unconfigured@null.spigotmc.org>
2017-08-14 17:29:55 +0300

    keep element IDs consistent

--- resources/views/settings/profile.blade.php
+++ resources/views/settings/profile.blade.php
@@ -6,9 +6,9 @@
 <form method="post" action="{{ route('settings.profile') }}">
     {{ csrf_field() }}
     <div class="form-group{{ $errors->has('first_name') ? ' has-error' : '' }}">
-        <label for="inputFirstName" class="control-label">{{ Helper::tra('settings.profile.first-name') }}</label>
+        <label for="first_name" class="control-label">{{ Helper::tra('settings.profile.first-name') }}</label>
 
-        <input type="text" class="form-control" id="inputFirstName" name="first_name" placeholder="Your first name" value="{{ Auth::user()->first_name }}">
+        <input type="text" class="form-control" id="first_name" name="first_name" placeholder="Your first name" value="{{ Auth::user()->first_name }}">
 
         @if ($errors->has('first_name'))
             <span class="help-block">
@@ -17,8 +17,8 @@
         @endif
     </div>
     <div class="form-group{{ $errors->has('last_name') ? ' has-error' : '' }}">
-        <label for="inputLastName" class="control-label">{{ Helper::tra('settings.profile.last-name') }}</label>
-        <input type="text" class="form-control" id="inputLastName" name="last_name" placeholder="Your last name" value="{{ Auth::user()->last_name }}">
+        <label for="last_name" class="control-label">{{ Helper::tra('settings.profile.last-name') }}</label>
+        <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Your last name" value="{{ Auth::user()->last_name }}">
 
         @if ($errors->has('last_name'))
             <span class="help-block">
@@ -27,8 +27,8 @@
         @endif
     </div>
     <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-        <label for="inputEmail" class="control-label">{{ Helper::tra('settings.profile.email') }}</label>
-        <input type="email" class="form-control" id="inputEmail" name="email" placeholder="Your email address" value="{{ Auth::user()->email }}">
+        <label for="email" class="control-label">{{ Helper::tra('settings.profile.email') }}</label>
+        <input type="email" class="form-control" id="email" name="email" placeholder="Your email address" value="{{ Auth::user()->email }}">
 
         @if ($errors->has('email'))
             <span class="help-block">
