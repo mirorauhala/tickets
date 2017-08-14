2072be56ab8ac2c298caa8bf35756432f7f0e023
BuildTools <unconfigured@null.spigotmc.org>
2017-08-14 18:12:53 +0300

    add new SettingsProfileTest and Page

---
+++ tests/Browser/Pages/SettingsProfilePage.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Browser\Pages;
+
+use Laravel\Dusk\Browser;
+
+class SettingsProfilePage extends Page
+{
+    /**
+     * Get the URL for the page.
+     *
+     * @return string
+     */
+    public function url()
+    {
+        return '/settings';
+    }
+
+    /**
+     * Assert that the browser is on the page.
+     *
+     * @return void
+     */
+    public function assert(Browser $browser)
+    {
+        $browser->assertPathIs($this->url());
+    }
+
+    /**
+     * Assert that user can sign up.
+     *
+     * @return void
+     */
+    public function updateProfile(Browser $browser,
+        $firstName = null,
+        $lastName = null,
+        $email = null,
+        $streetAddress = null,
+        $postalCode = null,
+        $postalOffice = null,
+        $countryCode = null
+    ) {
+        $browser->type('@first_name', $firstName)
+            ->type('@last_name', $lastName)
+            ->type('@email', $email)
+            ->type('@street_address', $streetAddress)
+            ->type('@postal_code', $postalCode)
+            ->type('@postal_office', $postalOffice)
+            ->select('@country_code', $countryCode)
+            ->press('Update');
+    }
+
+    /**
+     * Get the element shortcuts for the page.
+     *
+     * [1] If a CSS selector is not provided, Dusk will search for an input
+     *     field with the given name attribute.
+     *
+     * @return array
+     */
+    public function elements()
+    {
+        return [
+            '@first_name'               => '#first_name',
+            '@last_name'                => '#last_name',
+            '@email'                    => '#email',
+            '@street_address'           => '#street_address',
+            '@postal_code'              => '#postal_code',
+            '@postal_office'            => '#postal_office',
+            '@country_code'             => 'country_code', // [1]
+        ];
+    }
+}
---
+++ tests/Browser/Tests/SettingsTests/ProfileTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Browser\Tests\SettingsTests;
+
+use Tikematic\Models\User;
+use Tests\DuskTestCase;
+use Tests\Browser\Pages\SettingsProfilePage;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ProfileTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @test A user can update profile after sign up
+     *
+     * @return void
+     */
+    public function a_user_can_update_profile()
+    {
+
+        $user = factory(User::class)->create();
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->resize(1920, 1500); // Width and Height
+
+            $updated_details = [
+                'Lady',
+                'Gaga',
+                'lady.gaga@testing.com',
+                'Gagas street',
+                '00200',
+                'Postal Office GAGA',
+                'sv'
+            ];
+
+            $browser->loginAs($user)
+                    ->visit(new SettingsProfilePage)
+                    ->updateProfile(...$updated_details)
+                    ->assertInputValue('@first_name', 'Lady')
+                    ->assertInputValue('@last_name', 'Gaga')
+                    ->assertInputValue('@email', 'lady.gaga@testing.com')
+                    ->assertInputValue('@street_address', 'Gagas street')
+                    ->assertInputValue('@postal_code', '00200')
+                    ->assertInputValue('@postal_office', 'Postal Office GAGA')
+                    ->assertSelected('@country_code', 'sv');
+        });
+    }
+}
