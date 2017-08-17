7ad0278a2c47f3fa4e0ba3a8f148ff41bfdb9fcc
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-08-17 11:45:33 +0300

    fix ticket price formatting

--- resources/views/tickets/view.blade.php
+++ resources/views/tickets/view.blade.php
@@ -10,7 +10,7 @@
     <div class="col-md-8">
         <h1>{{ $order_item->title }}</h1>
         <ul class="list-unstyled">
-            <li><b>Hinta</b>: {{ $order_item->value }}</li>
+            <li><b>Hinta</b>: {{ Helper::decimalMoneyFormatter($order_item->value, "EUR") }}</li>
             <li><b>Pohjautuu tilaukseen</b>: <a href="{{ route('settings.orders.specific', ['order' => $order_item->order->reference]) }}">{{ $order_item->order->reference }}</li>
         </ul>
     </div>
