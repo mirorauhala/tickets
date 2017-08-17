d776985756ba47b8169f3a7b5fd95a0e194c1dc5
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-08-17 10:05:56 +0300

    remove crisp from auth pages, closes #48

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,13 +1,29 @@
-@extends('layouts.base')
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{{ asset('images/favicon.png') }}">
 
-@section('base.content')
+    <!-- CSRF Token -->
+    <meta id="csrfToken" content="{{ csrf_token() }}">
 
-@include('partials.nav.sidebar')
-<section class="application">
-    <section class="application-contents">
-        <div class="container">
-            @yield('content')
-        </div>
+    <title>{{ config('app.name', 'Tikematic') }} - @yield('base.title', 'Untitled')</title>
+
+    <!-- Styles -->
+    <meta name="theme-color" content="#FF3D78">
+    <link rel="manifest" href="/manifest.json">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" async="true">
+</head>
+<body>
+    @include('partials.nav.sidebar')
+    <section class="application">
+        <section class="application-contents">
+            <div class="container">
+                @yield('content')
+            </div>
+        </section>
     </section>
-</section>
-@endsection
+</body>
+</html>
