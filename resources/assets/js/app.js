0869c7cb5d1f247e2d4dfe37dde8d9cfaf2d4221
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-08-21 17:22:09 +0300

    remove crisp

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,14 +1,8 @@
 /**
- * Add Crisp
- */
-
-require('./crisp');
-/**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-
 require('./bootstrap');
 require('./tooltip');
 window.QRious = require('qrious');
