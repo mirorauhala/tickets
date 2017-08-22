fb869105792124aea1fa8a1067eef9c9c1885cca
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-08-22 17:04:10 +0300

    update unpaid ticket card design

--- resources/views/tickets/pending.blade.php
+++ resources/views/tickets/pending.blade.php
@@ -8,26 +8,39 @@
         <p class="lead">{{ Helper::tra('tickets.pending-message') }}</p>
     </div>
     @foreach($order_items as $order_item)
-        <div class="col-lg-5 col-md-6 col-sm-6">
-            <div class="ticket-card">
-                <div class="ticket-details">
+        <div class="col-lg-5 col-md-5 col-sm-5">
+            <div class="ticket">
+                <div class="ticket-heading">
                     <h1>{{ $order_item->title }}</h1>
-                    <p>{{ Helper::decimalMoneyFormatter($order_item->value, "EUR") }}</p>
                 </div>
 
-                @if($order_item->status == "pending")
-                    <a href="{{ route('settings.orders.specific', ['order' => $order_item->order->reference ]) }}" class="ticket-action">
-                        <span>Finish order</span>
+                <div class="ticket-meta">
+                    <p>{{ $order_item->order->event->name }}</p>
+                </div>
+
+                <div class="ticket-body">
+                    <div class="ticket-price">
+                        <h2>{{ Helper::tra('tickets.card.price') }}</h2>
+                        <p>{{ Helper::decimalMoneyFormatter($order_item->value, "EUR") }}</p>
+                    </div>
+
+                    <div class="ticket-seating-code">
+                        @if($order_item->seat)
+                            <h2>{{ Helper::tra('tickets.card.seating-code') }}</h2>
+                            <p>{{ $order_item->seat->name }}</p>
+                        @else
+                            <h2>{{ Helper::tra('tickets.card.seating-code') }}</h2>
+                            <p>-</p>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="ticket-actions">
+                    <a class="ticket-code" href="{{ route('settings.orders.specific', ['order' => $order_item->order->reference ]) }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path fill="#333" d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z" /></svg>
+                        <span>{{ Helper::tra('tickets.card.action-details') }}</span>
                     </a>
-                @elseif($order_item->status == "paid")
-                    <a href="{{ route('tickets.view', ['order' => $order_item->order->reference ]) }}" class="ticket-action">
-                        <span>View ticket</span>
-                    </a>
-                @elseif($order_item->status == "unassigned")
-                    <a href="#" class="ticket-action">
-                        <span>Assign</span>
-                    </a>
-                @endif
+                </div>
                 </a>
             </div>
         </div>
