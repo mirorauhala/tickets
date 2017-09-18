4c5a25ee18f7fc0d63caf5835928cacd66766765
Miro Rauhala <miro.rauhala.99@gmail.com>
2017-09-18 20:34:40 +0300

    rewrite PasswordController logic

--- app/Http/Controllers/User/Settings/PasswordController.php
+++ app/Http/Controllers/User/Settings/PasswordController.php
@@ -2,23 +2,25 @@
 
 namespace Tikematic\Http\Controllers\User\Settings;
 
-use Auth;
-use Hash;
 use Helper;
 use Illuminate\Http\Request;
 use Tikematic\Http\Requests\SettingsPasswordRequest;
 use Tikematic\Http\Controllers\Controller;
+use Tikematic\Repositories\Contracts\UserRepository;
 
 class PasswordController extends Controller
 {
+    protected $user;
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(UserRepository $user)
     {
         $this->middleware('auth');
+        $this->user = $user;
     }
 
     /**
@@ -38,13 +40,11 @@ class PasswordController extends Controller
      */
     public function updatePassword(SettingsPasswordRequest $request)
     {
-
-        // get the user model
-        $user = Auth::user();
-
-        // update the password
-        $user->password = $request->new_password;
-        $user->save();
+        // update password
+        $this->user->update(
+            $this->user->authenticated()->id,
+            ["password" => $request->new_password]
+        );
 
         // return to view with flash message
         return redirect()
