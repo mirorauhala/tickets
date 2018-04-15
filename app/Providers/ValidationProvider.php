00b01dc39669c2002eb08c0a08d793b3655e286d
Miro Rauhala <miro.rauhala.99@gmail.com>
2018-04-15 20:09:47 +0300

    remove validator

--- app/Providers/ValidationProvider.php
+++ app/Providers/ValidationProvider.php
@@ -14,7 +14,6 @@ class ValidationProvider extends ServiceProvider
      */
     public function boot()
     {
-        Validator::extend('validateTicketAvailabilityAtThisTime', 'App\Validators\OrderValidator@validateTicketAvailabilityAtThisTime');
         Validator::extend('validateSeatAvailability', 'App\Validators\OrderValidator@validateSeatAvailability');
         Validator::extend('validateOrderItemStatusAndSeatAvailability', 'App\Validators\OrderValidator@validateOrderItemStatusAndSeatAvailability');
     }
