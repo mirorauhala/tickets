399571f4b48aef2a19d71ad54abdc778fa54dff8
Miro Rauhala <miro.rauhala.99@gmail.com>
2018-04-23 14:47:56 +0300

    split factories

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Models\Event::class, function (Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'name'        => $faker->company,
+        'slug'        => $faker->word,
+        'location'    => $faker->address,
+        'slogan'      => $faker->text,
+        'url'         => $faker->url,
+    ];
+});
---
+++ database/factories/TicketFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Event;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Models\Ticket::class, function (Faker\Generator $faker) {
+    return [
+        'name'                    => $faker->sentence,
+        'price'                   => 1000,
+        'vat'                     => 10,
+        'reserved'                => 10,
+        'maxAmountPerTransaction' => 5,
+        'availableAt'             => \Carbon\Carbon::now(),
+        'unavailableAt'           => \Carbon\Carbon::now()->addWeek(),
+        'is_seatable'             => 1,
+        'event_id'                => function () {
+            return factory(Event::class)->create()->id;
+        },
+    ];
+});
