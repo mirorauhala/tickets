21510416dd3fe6dc9a49efb157bbf5fc6cc7df34
Miro Rauhala <miro.rauhala.99@gmail.com>
2018-10-17 01:16:37 +0300

    clean import order

--- app/Http/Controllers/Dashboard/TicketController.php
+++ app/Http/Controllers/Dashboard/TicketController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers\Dashboard;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\TicketRequest;
 use App\Models\Event;
 use App\Models\Ticket;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TicketRequest;
 
 class TicketController extends Controller
 {
