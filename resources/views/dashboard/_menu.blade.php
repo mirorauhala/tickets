89dd36f0eaab5126d03da3a0bc56b4b14eb96b95
Miro Rauhala <miro.rauhala.99@gmail.com>
2018-10-18 00:24:12 +0300

    add ticket.edit to view link active state

--- resources/views/dashboard/_menu.blade.php
+++ resources/views/dashboard/_menu.blade.php
@@ -7,7 +7,7 @@
         @if(active('dashboard.tickets*', [], true))
         <ul class="nav flex-column pl-3 py-3">
             <li class="nav-item">
-                <a class="nav-link{{ active(['dashboard.tickets', 'dashboard.tickets.view']) }}" href="{{ route('dashboard.tickets', $event) }}">View</a>
+                <a class="nav-link{{ active(['dashboard.tickets', 'dashboard.tickets.view', 'dashboard.tickets.edit']) }}" href="{{ route('dashboard.tickets', $event) }}">View</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link{{ active('dashboard.tickets.create') }}" href="{{ route('dashboard.tickets.create', $event) }}">Create</a>
