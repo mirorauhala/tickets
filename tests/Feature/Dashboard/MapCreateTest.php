3dd40ac5e7bc87aebf46dae1de602fd07c65f781
Miro Rauhala <miro.rauhala.99@gmail.com>
2018-10-21 18:36:43 +0300

    add Map dashboard tests

---
+++ tests/Feature/Dashboard/MapCreateTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature\Dashboard;
+
+use Tests\TestCase;
+use App\Models\Event;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class MapCreateTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $event;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->createUser();
+        $this->event = factory(Event::class)->create();
+        $this->user->events()->attach($this->event);
+        $this->uri = route('dashboard.maps.create', [$this->event->slug]);
+        $this->fields = [
+            'name' => 'Map of 2018 event',
+        ];
+    }
+
+    /** @test */
+    public function user_can_create_a_map()
+    {
+        $this->actingAs($this->user)->doRequest('post');
+
+        $this->response->assertRedirect();
+        $this->response->assertSessionMissing('errors');
+    }
+
+    /** @test */
+    public function map_name_is_required()
+    {
+        $this->fieldOverrides = [
+            'name' => '',
+        ];
+        $this->actingAs($this->user)->doRequest('post');
+
+        $this->response->assertRedirect();
+        $this->response->assertSessionHasErrors(['name']);
+    }
+}
