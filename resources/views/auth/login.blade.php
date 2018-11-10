42e87be16432f5572257b37c14160ee31fbeb72c
Miro Rauhala <miro.rauhala.99@gmail.com>
2018-11-10 20:51:07 +0200

    fix indentation

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,38 +13,40 @@
     <div class="row justify-content-center">
         <div class="col-md-6">
             <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-            {{ csrf_field() }}
+                {{ csrf_field() }}
 
-            <div class="form-group">
-                <label for="email">{{ tra('auth.login.email') }}</label>
-                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" autocomplete="email" required autofocus>
-                @if ($errors->has('email'))
-                    <div class="invalid-feedback">
-                        {{ $errors->first('email') }}
+                <div class="form-group">
+                    <label for="email">{{ tra('auth.login.email') }}</label>
+                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" autocomplete="email" required autofocus>
+                    @if ($errors->has('email'))
+                        <div class="invalid-feedback">
+                            {{ $errors->first('email') }}
+                        </div>
+                    @endif
+                </div>
+
+                <div class="form-group">
+                    <label for="password">{{ tra('auth.login.password') }}</label>
+                    <input id="password" type="password" class="form-control" name="password" autocomplete="current-password" required>
+                </div>
+
+                <div class="form-group">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" name="remember" id="rememberMe"{{ old('remember') ? ' checked' : '' }}>
+                        <label class="form-check-label" for="rememberMe">
+                            {{ tra('auth.login.remember-me') }}
+                        </label>
                     </div>
-                @endif
-            </div>
+                </div>
 
-            <div class="form-group">
-                <label for="password">{{ tra('auth.login.password') }}</label>
-                <input id="password" type="password" class="form-control" name="password" autocomplete="current-password" required>
-            </div>
-
-        <div class="form-group">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" name="remember" id="rememberMe"{{ old('remember') ? ' checked' : '' }}>
-                <label class="form-check-label" for="rememberMe">
-                    {{ tra('auth.login.remember-me') }}
-                </label>
-            </div>
+                <div class="form-group align-items-center d-flex">
+                    <a href="{{ route('password.request') }}" class="d-inline-block">
+                        {{ tra('auth.login.forgot-password') }}
+                    </a>
+                    <input type="submit" class="btn px-4 ml-auto btn-primary" value="{{ tra('auth.login.login') }}">
+                </div>
+            </form>
         </div>
-
-        <div class="form-group align-items-center d-flex">
-            <a href="{{ route('password.request') }}" class="d-inline-block">
-                {{ tra('auth.login.forgot-password') }}
-            </a>
-            <input type="submit" class="btn px-4 ml-auto btn-primary" value="{{ tra('auth.login.login') }}">
-        </div>
-    </form>
+    </div>
 </div>
 @endsection
