3f9233ed9f35bdd4235b602a135f871042a2e4d9
Miro Rauhala <miro.rauhala.99@gmail.com>
2018-11-12 22:46:47 +0200

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,9 @@
-if [-d vendor/]
-then
-    php artisan down
-fi
-
+#!/bin/bash
+php artisan down
 composer install
 php artisan migrate
 php artisan db:seed
 php artisan config:cache
 php artisan view:cache
 php artisan optimize
-
-if [-d vendor/]
-then
-    php artisan up
-fi
+php artisan up
