87ec6425bd5bb9e8542c123a2ebc5f313c40610c
Miro Rauhala <miro.rauhala.99@gmail.com>
2018-11-14 21:13:44 +0200

    empty phone would trigger as unique
    there's really no need for phones to be unique

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
-            $table->string('phone')->unique()->nullable();
+            $table->string('phone')->nullable();
             $table->string('language')->nullable();
             $table->string('password');
             $table->string('superuser')->default(0);
