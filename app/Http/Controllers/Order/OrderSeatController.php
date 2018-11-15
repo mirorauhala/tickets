9d3fd0524c3b25bed1ca11d0502623685bc1bddc
Miro Rauhala <miro.rauhala.99@gmail.com>
2018-11-15 19:23:46 +0200

    cs: remove unused imports

--- app/Http/Controllers/Order/OrderSeatController.php
+++ app/Http/Controllers/Order/OrderSeatController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Order;
 
 use App\Models\Seat;
 use App\Models\Event;
-use App\Models\Order;
 use App\Models\OrderItem;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\OrderSeatRequest;
--- app/Http/Requests/OrderSeatRequest.php
+++ app/Http/Requests/OrderSeatRequest.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Requests;
 
-use App\Models\Seat;
 use Illuminate\Foundation\Http\FormRequest;
 
 class OrderSeatRequest extends FormRequest
