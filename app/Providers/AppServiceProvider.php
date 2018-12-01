1427f8e5dc7832abece80d407b469daa6632eada
Miro Rauhala <miro.rauhala.99@gmail.com>
2018-12-01 15:32:20 +0200

    switch debugbar to clockwork

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Laravel\Dusk\DuskServiceProvider;
 use Illuminate\Support\ServiceProvider;
-use Barryvdh\Debugbar\ServiceProvider as DebugbarServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,7 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         if ($this->app->environment('local', 'testing')) {
-            $this->app->register(DebugbarServiceProvider::class);
             $this->app->register(DuskServiceProvider::class);
         }
     }
