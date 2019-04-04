ec2b5a33ce85e8e77b5731e2f7ac75f548161ecf
Miro Rauhala <mirorauhala@users.noreply.github.com>
2019-04-04 09:01:25 +0300

    remove unnecessary comments

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,18 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="{{ asset('images/favicon.png') }}">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Tickets') }} - @yield('base.title', 'Untitled')</title>
 
-    <!-- Styles -->
     <meta name="theme-color" content="#2A5CFF">
     <link rel="manifest" href="/manifest.json">
     <link href="{{ mix('css/app.css') }}" integrity="{{ Sri::hash('css/app.css') }}" crossorigin="anonymous" rel="stylesheet">
 
-    <!-- SHA1: {{ current_commit() }} -->
 </head>
 <body class="bg-light">
     <div id="app">
