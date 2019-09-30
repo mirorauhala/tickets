38cddbba9933d5431e702c205b54684be112130d
Miro Rauhala <mirorauhala@users.noreply.github.com>
2019-09-30 15:18:19 +0300

    add new order factories

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+use Carbon\Carbon;
+use App\Models\User;
+use App\Models\Event;
+use App\Models\Order;
+use Faker\Generator as Faker;
+
+$factory->define(Order::class, function (Faker $faker) {
+    return [
+        'reference'          => strtoupper(bin2hex(openssl_random_pseudo_bytes(6))),
+        'currency'           => 'EUR',
+        'value'              => 10000,
+        'vat'                => 14,
+        'status'             => 'paid',
+        'payer_name'         => $faker->name,
+        'release_lock_after' => Carbon::now(),
+        'event_id'           => function () {
+            return factory(Event::class)->create()->id;
+        },
+        'user_id' => function () {
+            return factory(User::class)->create()->id;
+        },
+    ];
+});
---
+++ database/factories/OrderItem.php
@@ -0,0 +1,28 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+use Carbon\Carbon;
+use App\Models\User;
+use App\Models\Order;
+use App\Models\Ticket;
+use App\Models\OrderItem;
+use Faker\Generator as Faker;
+
+$factory->define(OrderItem::class, function (Faker $faker) {
+    return [
+        'title'              => $faker->title,
+        'barcode'            => strtoupper(bin2hex(openssl_random_pseudo_bytes(6))),
+        'value'              => 1000,
+        'status'             => 'paid',
+        'release_lock_after' => Carbon::now(),
+        'ticket_id'          => function () {
+            return factory(Ticket::class)->create()->id;
+        },
+        'user_id' => function () {
+            return factory(User::class)->create()->id;
+        },
+        'order_id' => function () {
+            return factory(Order::class)->create()->id;
+        },
+    ];
+});
