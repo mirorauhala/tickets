a0d1dd79f060794f1a919dd83aa9315887ea6a0f
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2020-02-04 17:49:28 +0200

    fix event card

--- resources/views/events/featured.blade.php
+++ resources/views/events/featured.blade.php
@@ -3,28 +3,17 @@
 @section('base.title', tra('featured.title'))
 
 @section('base.content')
-<div class="tw-container tw-mx-auto">
-    <div class="row pt-4">
-        <div class="col-md-12">
-            <h1>{{ tra('featured.title') }}</h1>
-        </div>
-        <div class="col-md-12">
-            @include('events._menu')
-        </div>
-    </div>
-    <div class="row">
+<div class="container mx-auto">
+    <div class="w-7/12 mx-auto">
+        <h1 class="text-5xl font-bold mb-3">{{ tra('featured.title') }}</h1>
+        @include('events._menu')
         @if(count($events) > 0)
             @foreach($events as $event)
-            <div class="col-md-6 col-lg-6">
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $event->name }}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{{ $event->location }}</h6>
-                        <p class="card-text">{{ $event->slogan }}</p>
-                        <a href="{{ route('events.show', ['event' => $event]) }}" class="card-link">{{ tra('featured.card-cta') }}</a>
-                    </div>
-                </div>
-            </div>
+                <a class="block bg-white border border-solid border-gray-300 rounded mb-3 px-10 py-6" href="{{ route('events.show', ['event' => $event]) }}">
+                    <h2 class="text-3xl font-bold">{{ $event->name }}</h2>
+                    <p class="text-xl text-gray-600">{{ $event->location }}</p>
+                    <p class="text-lg">{{ $event->slogan }}</p>
+                </a>
             @endforeach
         @else
             <p class="lead">{{ tra('featured.no-events') }}</p>
