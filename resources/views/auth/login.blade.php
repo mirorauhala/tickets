fb699faa303eb9038e2fb5288b0f3d4366b88351
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2020-02-04 17:26:11 +0200

    use tailwind on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,50 +3,46 @@
 @section('base.title', tra('auth.login.title'))
 
 @section('base.content')
-<div class="container h-100">
-    <div class="row py-5 justify-content-center align-items-center">
-        <div class="col-md-6">
-            <h1>{{ tra('auth.login.title') }}</h1>
-            <p class="lead">{{ tra('auth.login.lead') }}</p>
-        </div>
+<div class="container mx-auto">
+    <div class="w-full">
+        <h1 class="text-5xl font-bold">{{ tra('auth.login.title') }}</h1>
+        <p class="text-lg text-gray-700">{{ tra('auth.login.lead') }}</p>
     </div>
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-                {{ csrf_field() }}
+    <div class="w-full">
+        <form role="form" method="POST" action="{{ route('login') }}">
+            {{ csrf_field() }}
 
-                <div class="form-group">
-                    <label for="email">{{ tra('auth.login.email') }}</label>
-                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" autocomplete="email" required autofocus>
-                    @if ($errors->has('email'))
-                        <div class="invalid-feedback">
-                            {{ $errors->first('email') }}
-                        </div>
-                    @endif
-                </div>
-
-                <div class="form-group">
-                    <label for="password">{{ tra('auth.login.password') }}</label>
-                    <input id="password" type="password" class="form-control" name="password" autocomplete="current-password" required>
-                </div>
-
-                <div class="form-group">
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="remember" id="rememberMe"{{ old('remember') ? ' checked' : '' }}>
-                        <label class="form-check-label" for="rememberMe">
-                            {{ tra('auth.login.remember-me') }}
-                        </label>
+            <div class="mb-2">
+                <label for="email" class="font-bold">{{ tra('auth.login.email') }}</label>
+                <form-input id="email" type="email" :error="{{ $errors->has('email') ? 'true' : 'false' }}" name="email" value="{{ old('email') }}" autocomplete="email" required autofocus>
+                @if ($errors->has('email'))
+                    <div class="invalid-feedback">
+                        {{ $errors->first('email') }}
                     </div>
-                </div>
+                @endif
+            </div>
 
-                <div class="form-group align-items-center d-flex">
-                    <a href="{{ route('password.request') }}" class="d-inline-block">
-                        {{ tra('auth.login.forgot-password') }}
-                    </a>
-                    <input type="submit" class="btn px-4 ml-auto btn-primary" value="{{ tra('auth.login.login') }}">
+            <div class="mb-2">
+                <label for="password" class="font-bold">{{ tra('auth.login.password') }}</label>
+                <form-input id="password" type="password" class="block px-3 py-2 border rounded" name="password" autocomplete="current-password" required>
+            </div>
+
+            <div class="mb-2">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="remember" id="rememberMe"{{ old('remember') ? ' checked' : '' }}>
+                    <label class="form-check-label" for="rememberMe">
+                        {{ tra('auth.login.remember-me') }}
+                    </label>
                 </div>
-            </form>
-        </div>
+            </div>
+
+            <div class="mb-2 align-items-center d-flex">
+                <a href="{{ route('password.request') }}" class="d-inline-block">
+                    {{ tra('auth.login.forgot-password') }}
+                </a>
+                <input type="submit" class="btn px-4 ml-auto btn-primary" value="{{ tra('auth.login.login') }}">
+            </div>
+        </form>
     </div>
 </div>
 @endsection
