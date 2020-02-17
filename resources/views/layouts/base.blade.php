d5f7c014db9c4915878c847b17a481fe562625a7
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2020-02-17 18:16:21 +0200

    laravel-sri is fixed

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="icon" href="{{ asset('images/favicon.png') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -13,9 +13,7 @@
     <link rel="manifest" href="/manifest.json">
 
     @if(app()->environment('production'))
-    {{-- Bug in laravel-sri --}}
-    {{-- integrity="{{ Sri::hash('css/app.css') }}" crossorigin="anonymous"  --}}
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet" integrity="{{ Sri::hash('css/app.css') }}" crossorigin="anonymous">
     @else
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     @endif
@@ -29,9 +27,7 @@
     </div>
 
     @if(app()->environment('production'))
-    {{-- Bug in laravel-sri --}}
-    {{-- integrity="{{ Sri::hash('css/app.css') }}" crossorigin="anonymous"  --}}
-    <script src="{{ mix('js/app.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}" integrity="{{ Sri::hash('css/app.css') }}" crossorigin="anonymous"></script>
     @else
     <script src="{{ mix('js/app.js') }}"></script>
     @endif
