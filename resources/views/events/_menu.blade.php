76f496e1ca97752a856887a034ec28680b7fe8b5
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2020-02-18 19:27:10 +0200

    create pills menu

--- resources/views/events/_menu.blade.php
+++ resources/views/events/_menu.blade.php
@@ -1,10 +1,10 @@
-<nav class="mb-5">
-    <ul class="flex">
-        <li class="pr-3">
-            <a class="inline-block rounded-lg bg-blue-100 shadow font-medium text-blue-500 py-1 px-3{{ active('home') }}" href="{{ route('home') }}">Featured</a>
+<nav>
+    <ul class="nav nav-pills">
+        <li class="nav-item">
+            <a class="nav-link {{ active('home') }}" href="{{ route('home') }}">Featured</a>
         </li>
-        <li class="pr-3">
-            <a class="inline-block rounded-lg bg-white border border-gray-300 py-1 px-3{{ active('events.index') }}" href="{{ route('events.index') }}">All</a>
+        <li class="nav-item">
+            <a class="nav-link {{ active('events.index') }}" href="{{ route('events.index') }}">All</a>
         </li>
     </ul>
 </nav>
