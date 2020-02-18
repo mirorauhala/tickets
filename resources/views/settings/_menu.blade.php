091a612f8d29ae4a5a7dc75ecb204a5680709357
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2020-02-18 19:30:21 +0200

    pills menu

--- resources/views/settings/_menu.blade.php
+++ resources/views/settings/_menu.blade.php
@@ -1,16 +1,16 @@
-<nav class="mb-5 pr-8">
-    <ul class="flex flex-col">
-        <li>
-            <a class="block text-lg rounded-lg font-medium bg-white py-2 {{ active('settings') }}" href="{{ route('settings') }}">{{ tra('settings.menu.account') }}</a>
+<nav>
+    <ul class="nav nav-pills">
+        <li class="nav-item">
+            <a class="nav-link {{ active('settings') }}" href="{{ route('settings') }}">{{ tra('settings.menu.account') }}</a>
         </li>
-        <li>
-            <a class="block text-lg rounded-lg font-medium bg-white py-2 {{ active('settings.password') }}" href="{{ route('settings.password') }}">{{ tra('settings.menu.password') }}</a>
+        <li class="nav-item">
+            <a class="nav-link {{ active('settings.password') }}" href="{{ route('settings.password') }}">{{ tra('settings.menu.password') }}</a>
         </li>
-        <li>
-            <a class="block text-lg rounded-lg font-medium bg-white py-2 {{ active('settings.language') }}" href="{{ route('settings.language') }}">{{ tra('settings.menu.language') }}</a>
+        <li class="nav-item">
+            <a class="nav-link {{ active('settings.language') }}" href="{{ route('settings.language') }}">{{ tra('settings.menu.language') }}</a>
         </li>
-        <li>
-            <a class="block text-lg rounded-lg font-medium bg-white py-2 {{ active('settings.about') }}" href="{{ route('settings.about') }}">{{ tra('settings.menu.about') }}</a>
+        <li class="nav-item">
+            <a class="nav-link {{ active('settings.about') }}" href="{{ route('settings.about') }}">{{ tra('settings.menu.about') }}</a>
         </li>
     </ul>
 </nav>
